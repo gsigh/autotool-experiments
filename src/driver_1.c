82551b315cc70fa707d1e5ba10a07b1468ff3205
Gerhard Sittig <gerhard.sittig@gmx.net>
2019-12-18 21:52:58 +0100

    driver list: more driver code, unbreak .ld file ref, dependencies
    Add "more code" to driver sources so that there is not just the
    registration detail. Unbreak the references to the linker script to
    unbreak out-of-source builds. Complete the dependency declarations.
    Unassorted linker script nits (avoid .rodata for now).

--- src/driver_1.c
+++ src/driver_1.c
@@ -1,3 +1,10 @@
+#include <stdio.h>
 #include "drivers.h"
+
+static void local_func(void) __attribute__((used));
+static void local_func(void)
+{
+	printf("driver1, do something\n");
+}
 
 DECLARE_DRIVER(driver1, 1)
--- src/driver_2.c
+++ src/driver_2.c
@@ -1,3 +1,10 @@
+#include <stdio.h>
 #include "drivers.h"
+
+static void local_func(void) __attribute__((used));
+static void local_func(void)
+{
+	printf("driver2, do something");
+}
 
 DECLARE_DRIVER(driver2, 2)
