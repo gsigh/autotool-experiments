10f7e3dacbfa1b7409a58ff05a2e963b18407d65
Gerhard Sittig <gerhard.sittig@gmx.net>
2019-12-19 12:01:41 +0100

    driver list: avoid unexpected NULL base address, improve extern decl
    Declaring a pointer seems to also create the variable and initialize its
    value to zero. Delcaring an external array instead does result in the
    absence of the variable "in code", and does reference the linker
    provided symbol instead, which is the address of a memory range.
    
    Print the start and end address of the drivers list before iteration. To
    spot when the list is "terminated at its beginning" although more data
    is available.

--- src/drivers.c
+++ src/drivers.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "drivers.h"
 
-extern const int **drivers_list;
+extern const int *drivers_list[];
+extern const int *drivers_end[];
 
 void iterate_drivers(void)
 {
 	const int **p, *d;
 
+	printf("refs: %p %p\n", drivers_list, drivers_end);
 	printf("drivers:");
 	p = drivers_list;
 	printf(" %p", p);
